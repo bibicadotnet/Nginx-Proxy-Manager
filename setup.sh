b818a94073ee14463a6b2b26f11ef2a0c54ddfcc
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-14 09:28:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $(whoami)
 sudo systemctl start docker
 sudo systemctl enable docker
-apt  install docker-compose -y
+apt install docker-compose -y
 mkdir /root/nginxproxymanager
 cd /root/nginxproxymanager
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Nginx-Proxy-Manager/main/docker-compose.yml -O /root/nginxproxymanager/docker-compose.yml
