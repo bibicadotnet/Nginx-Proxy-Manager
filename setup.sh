f3a3b5bd8c8fa7031f029d3b7182f46aa0031946
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-16 21:20:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt remove iptables-persistent -y && sudo ufw disable && sudo iptables -F
-sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $(whoami)
 sudo systemctl start docker
